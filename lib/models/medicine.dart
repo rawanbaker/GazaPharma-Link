f7a8aec9185c9abd6fb020c5ff2b194b2bf2785b
kamlahd <kamlahader1234@gmail.com>
2026-05-18 10:37:46 +0300

    Create medicine.dart

---
+++ lib/models/medicine.dart
@@ -0,0 +1,8 @@
+class Medicine {
+  final String name;
+  final String genericName;
+  final double strength;
+  final String dosageForm;
+
+  Medicine({required this.name, required this.genericName, required this.strength, required this.dosageForm});
+}
